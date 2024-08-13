8dbb4d9a6846d6b1f2007e30806176f116780f6e
MrCache <107826098+MrCache1209@users.noreply.github.com>
2024-08-13 14:30:11 +0930

    Update Unix-Patcher.sh

--- Unix-Patcher.sh
+++ Unix-Patcher.sh
@@ -12,10 +12,13 @@ chmod 755 /root/admin/update.sh
 # Create the log directory
 mkdir /root/admin/log
 
+# CleanUp Unix-Patching
+rm -rf Unix-Patching/
+
+# CleanUp Unix-Patching
+rm -rf Unix-Patching/
+
 # Add the new cron job to the crontab
 CRON_ENTRY="00 00 */8,*/9,*/10,*/11,*/12,*/13,*/14 * 4 /root/admin/update.sh 2>&1 >> /root/admin/log/update.log"
 # Check if the entry already exists to avoid duplication
 crontab -l | grep -F "$CRON_ENTRY" || (crontab -l; echo "$CRON_ENTRY") | crontab -
-
-# CleanUp Unix-Patching
-rm -rf Unix-Patching/
